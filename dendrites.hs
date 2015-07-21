2c31f3fde85a1a0d89a93358d8100b1b5cee0b32
Matt Neary <neary.matt@gmail.com>
2015-07-20 23:50:47 -0400

    create axons and axon pipes

--- dendrites.hs
+++ dendrites.hs
@@ -3,29 +3,60 @@ module Main where
 import GHC.IO.Handle.FD
 import System.IO
 import Control.Concurrent
-import System.Posix.Files (createNamedPipe)
+import System.Environment
+import System.Posix.Files (createNamedPipe, accessModes)
+import System.Process (createProcess, proc)
 
 handleMsg s =
   do putStr "msg: "
      putStrLn s
 
-readMsg =
-  do h <- openFileBlocking "fifo" ReadMode
-     done <- hIsEOF h
+readMsg h =
+  do done <- hIsEOF h
      if done
      then return ()
      else 
        do c <- hGetLine h
           handleMsg c
 
-readLoop =
-  do readMsg
-     readLoop
+readLoop h =
+  do readMsg h
+     readLoop h
+     return ()
+
+data Pipe = Pipe String Handle
+data Axon = Axon String String String
+data AxonPipe = AxonPipe String Pipe Pipe
+
+axonPipe :: Axon -> IO AxonPipe
+axonPipe (Axon n r w) =
+  do createNamedPipe r accessModes
+     createNamedPipe w accessModes
+     -- Waits for fifos to have other managers before proceeding
+     wP <- openFileBlocking w ReadMode
+     rP <- openFileBlocking r WriteMode
+     return $ AxonPipe n (Pipe r rP) (Pipe w wP)
+
+makeAxon n =
+  do let r = n ++ "-r"
+     let w = n ++ "-w"
+     return $ Axon n r w
+
+connectAxons axons =
+  do axonPipes <- mapM axonPipe axons
+     mapM (\(AxonPipe _ _ (Pipe _ w)) -> readLoop w) axonPipes
+     return ()
+
+spinUpAxons axons =
+  do mapM (\(Axon name r w) ->
+       createProcess (proc name [r, w])) axons
+     return ()
 
 main =
-  do loopDone <- newEmptyMVar
-     forkIO $ readLoop >> putMVar loopDone ()
-     putStrLn "ready"
-     takeMVar loopDone
-     
+  do names <- getArgs
+     axons <- mapM makeAxon names
+     ran <- newEmptyMVar
+     forkIO $ connectAxons axons >> putMVar ran ()
+     forkIO $ spinUpAxons axons
+     takeMVar ran
 
